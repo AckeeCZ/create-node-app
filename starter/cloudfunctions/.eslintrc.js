3b6d34e455bf3bb2a694e1c4f81827a8cdfce1fe
pavelsvagr <pavel.svagr@ackee.cz>
2024-07-19 12:30:31 +0200

    👷Use lint inside starter folders as roots

--- starter/cloudfunctions/.eslintrc.js
+++ starter/cloudfunctions/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('@ackee/styleguide-backend-config/eslint'),
+  root: true,
   ignorePatterns: ['lib'],
   parserOptions: {
     project: '.eslint.tsconfig.json',
--- starter/cloudrun/.eslintrc.js
+++ starter/cloudrun/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('@ackee/styleguide-backend-config/eslint'),
+  root: true,
   ignorePatterns: ['dist', 'src/openapi', 'docs'],
   parserOptions: {
     project: '.eslint.tsconfig.json',
