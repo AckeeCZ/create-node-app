be3742a8fb8c2a0d5f6538ce3f5abc2c8fe7c3c7
Pavel Švagr <s.pavel66@gmail.com>
2025-08-27 21:54:25 +0200

    ✨ Add file mergers

---
+++ src/Mergers/EnvJsoncMerger.ts
@@ -0,0 +1,24 @@
+import { Merger } from './Merger.js'
+import fsp from 'fs/promises'
+
+export class EnvJsoncMerger extends Merger {
+  async merge(originDir: string): Promise<string> {
+    const content = await this.getWhichExistsOrNull(originDir)
+    if (content) {
+      return content
+    }
+
+    const { originPath, destPath } = this.getPaths(originDir)
+
+    const [destEnvConfig, originEnvConfig] = await Promise.all([
+      fsp.readFile(destPath, 'utf8'),
+      fsp.readFile(originPath, 'utf8'),
+    ])
+
+    const originWithoutOpenBracket = originEnvConfig.replace('{\n', '')
+
+    return destEnvConfig
+      .replace(',\n}\n', '\n}\n')
+      .replace('\n}\n', `,\n${originWithoutOpenBracket}`)
+  }
+}
